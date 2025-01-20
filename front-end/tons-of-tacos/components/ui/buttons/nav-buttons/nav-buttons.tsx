212378e87cea5c147c09ff42b1d3cd7a8e452592
Brian Crookshanks <zeron30@hotmail.com>
2025-01-20 13:00:29 -0700

    file cleanup

--- front-end/tons-of-tacos/components/ui/buttons/nav-buttons/nav-buttons.tsx
+++ front-end/tons-of-tacos/components/ui/buttons/nav-buttons/nav-buttons.tsx
@@ -6,7 +6,7 @@ import MenuNav from "../../../menu/menu-navigation/menu-navigation";
 import CartIcon from "./cart-icon";
 import CartQuantity from "../../badges/cart-quantity";
 import { useDisplayContext } from "@/context/display-context";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import Cart from "@/components/cart/cart";
 import { useCartContext } from "@/context/cart-context";
 import { GetCart } from "@/lib/cart";
@@ -15,9 +15,6 @@ export default function NavButtons(menuOptions: { menuOptions: Category[] }) {
   const { setMenuCategories } = useMenuCategoryContext();
   const { showMenu, setShowMenu, showCart, setShowCart } = useDisplayContext();
   const { setCart, cartQuantity } = useCartContext();
-
-  // const menuRef = useRef<boolean>(showMenu);
-  // const cartRef = useRef<boolean>(showCart);
 
   function toggleMenu() {
     setShowCart(false);
