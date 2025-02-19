670130e101c14e392124191af8db0bbab2f95f82
Brian Crookshanks <zeron30@hotmail.com>
2025-02-19 06:29:22 -0700

    default size and price update

--- front-end/tons-of-tacos/components/menu/menu-items/size-selector/size-selector.tsx
+++ front-end/tons-of-tacos/components/menu/menu-items/size-selector/size-selector.tsx
@@ -1,19 +1,29 @@
 import classes from "@/components/menu/menu-items/size-selector/size-selector.module.css";
-import SizeButton from "@/components/ui/buttons/radio-buttons/size-button";
+import SizeButton from "@/components/ui/buttons/size-buttons/size-button";
 
 export default function SizeSelector(props: {
   sizes: string[];
   sizeAvailable: boolean;
+  id: string;
 }) {
   return (
-    <section
+    // <section
+    //   className={`${classes.selectorGroup} ${
+    //     props.sizeAvailable === false ? classes.notShowing : " "
+    //   }`}
+    // >
+    //   {props.sizes.map((size: string) => (
+    //     <SizeButton key={size} size={size} />
+    //   ))}
+    // </section>
+    <ul
       className={`${classes.selectorGroup} ${
         props.sizeAvailable === false ? classes.notShowing : " "
       }`}
     >
       {props.sizes.map((size: string) => (
-        <SizeButton key={size} size={size} />
+        <SizeButton key={size} size={size} id={props.id} />
       ))}
-    </section>
+    </ul>
   );
 }
