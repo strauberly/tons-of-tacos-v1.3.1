6c8f25beef273204fc5b81581ea070a49b92045e
Brian Crookshanks <zeron30@hotmail.com>
2025-03-29 16:39:29 -0700

    owners dashboard initialization

--- front-end/tons-of-tacos/app/owners-tools/page.tsx
+++ front-end/tons-of-tacos/app/owners-tools/page.tsx
@@ -1,26 +1,18 @@
 "use client";
 
 import { useDisplayContext } from "@/context/display-context";
-import { useOwnerContext } from "@/context/owner-context";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 export default function OwnersTools() {
   const { setShowLogin } = useDisplayContext();
-  const { ownerName } = useOwnerContext();
-
-  console.log(ownerName);
-
-  // const name = useRef<string>();
 
   useEffect(() => {
     setShowLogin(true);
-    // name.current = ownerName;
   });
 
   return (
     <div>
       <p>Owners Tools</p>
-      <p>{ownerName}</p>
     </div>
   );
 }
